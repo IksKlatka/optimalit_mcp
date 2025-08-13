5bd1a0573583c669e8cade5b32c2405c0bb0d194
Iks <iga.klatka@gmail.com>
2025-08-13 13:13:14 +0200

    configs from app/

---
+++ app/config.py
@@ -0,0 +1,48 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CALENDAR_CLIENT_ID", False)
+GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CALENDAR_CLIENT_SECRET", False)
+GOOGLE_API_KEY = os.getenv("GOOGLE_CALENDAR_API_KEY", False)
+GOOGLE_CREDENTIALS_FILE = os.getenv("GOOGLE_CREDENTIALS_FILE", "credentials.json")
+
+API_KEY = os.getenv("API_KEY", False)
+GOOGLE_EMAIL_PASSWORD = os.getenv("GOOGLE_EMAIL_PASSWORD", False)
+GOOGLE_EMAIL_USER = os.getenv("GOOGLE_EMAIL_USER", False)
+SMSAPI_TOKEN = os.getenv("SMSAPI_TOKEN", False)
+
+SUPABASE_HOST = os.getenv("SUPABASE_HOST", "localhost")
+SUPABASE_PORT = os.getenv("SUPABASE_PORT", 5432 )
+SUPABASE_USER = os.getenv("SUPABASE_USER", "postgres")
+SUPABASE_PASSWORD = os.getenv("SUPABASE_PASSWORD", False)
+SUPABASE_DB = os.getenv("SUPABASE_DB", "postgres")
+
+COMPANY_HEADQUARTERS=os.getenv("COMPANY_HEADQUARTERS", "Plein 2A, 3861 AJ Nijkerk, Holandia")
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+
+def load_credentials():
+    """
+    Loads Google credentials from file.
+    Checks both token and updates data if something is missing.
+    :return: None
+    """
+
+    creds = Credentials.from_authorized_user_file(
+        GOOGLE_CREDENTIALS_FILE,
+        ["https://www.googleapis.com/auth/calendar"]
+    )
+
+    if creds.expired and creds.refresh_token:
+        # logger.info("Token expired, refreshing...")
+        creds.refresh(Request())
+        with open(GOOGLE_CREDENTIALS_FILE, "w") as token_file:
+            token_file.write(creds.to_json())
+    elif creds.expired and not creds.refresh_token:
+        raise Exception("Token expired and no refresh token — you need to log in again")
+
+    return creds
